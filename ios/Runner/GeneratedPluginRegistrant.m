e3f4ec72a7670f4f1f8323716fce73575e55a9ca
Chatali Pant <chatali.pant@debutinfotech.com>
2021-06-16 09:53:11 +0530

    Revert "initial commit"
    sync

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<fitness/FitnessPlugin.h>)
+#import <fitness/FitnessPlugin.h>
+#else
+@import fitness;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FitnessPlugin registerWithRegistrar:[registry registrarForPlugin:@"FitnessPlugin"]];
+}
+
+@end
